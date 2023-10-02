091b23ba03084ee0b11cc825e36b836996bcc274
David <davidhenriquehy@gmail.com>
2023-10-02 11:50:23 -0300

    controle dinamico do componente campaings

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,51 @@
 import Carrossel from "@/components/Carrossel"
 import SectionPhoto from "@/components/SectionPhoto"
 import Campaigns from "@/components/Campaigns"
+
+import Natal_valor_de_uma_vida from '../../public/images/cards carrosel/campaigns_Item_01.png'
+import Inverno_Solidario_valor_de_uma_vida from '../../public/images/cards carrosel/campaigns_Item_02.png'
+import dia_das_criancas_valor_de_uma_vida from '../../public/images/cards carrosel/campaigns_Item_03.png'
+
+import icon_natal_valor_de_uma_vida from '../../public/images/icon-natal-valor-de-uma-vida.svg'
+import icon_inverno_valor_de_uma_vida from '../../public/images/icon_SnowMan.svg'
+import icon_dia_das_criancas_valor_de_uma_vida from '../../public/images/icon_Teddy_Bear.svg'
+
+import arrow_icon_rose from '../../public/images/btn_Icon-rose.svg'
+import arrow_icon_blue from '../../public/images/btn_Icon-blue.svg'
+import arrow_icon_emerald from '../../public/images/btn_btn_Icon-emerald.svg'
+
+
+const natal = {
+  title: 'natal solidário',
+  photos: [Natal_valor_de_uma_vida, Natal_valor_de_uma_vida, Natal_valor_de_uma_vida],
+  icon: icon_natal_valor_de_uma_vida,
+  arrowIcon: arrow_icon_rose,
+  alt: 'natal solidário valor de uma vida'
+}
+
+const inverno = {
+  title: 'inverno solidário',
+  photos: [Inverno_Solidario_valor_de_uma_vida, Inverno_Solidario_valor_de_uma_vida, Inverno_Solidario_valor_de_uma_vida],
+  icon: icon_inverno_valor_de_uma_vida,
+  arrowIcon: arrow_icon_blue,
+  alt: 'inverno solidário valor de uma vida'
+
+}
+
+const diaDasCriancas = {
+  title: 'dia das crianças',
+  photos: [dia_das_criancas_valor_de_uma_vida, dia_das_criancas_valor_de_uma_vida, dia_das_criancas_valor_de_uma_vida],
+  icon: icon_dia_das_criancas_valor_de_uma_vida,
+  arrowIcon: arrow_icon_emerald,
+  alt: 'dia das crianças solidário valor de uma vida'
+}
+
 export default function Home() {
+
   return (
+
     <>
+
       <Carrossel title={'campanhas'} type={'card'} className={'w-[1216px] h-[515px] ml-auto mr-auto '} />
 
       <SectionPhoto />
@@ -14,7 +56,12 @@ export default function Home() {
 
         <h1 className="w-[484px] h-[60px] mr-auto ml-auto border-b-4 border-stone-900 text-black text-[38px] font-bold font-sans uppercase leading-[76.50px]">Campanhas realizadas</h1>
 
-        <Campaigns />
+        <Campaigns campaign={natal} mainColor={'rose-500'} fontColor={'text-rose-500'} />
+
+        <Campaigns campaign={inverno} mainColor={'sky-800'} fontColor={'text-sky-800'} />
+
+        <Campaigns campaign={diaDasCriancas} mainColor={'teal-300'} fontColor={'text-emerald-300'} />
+
 
       </section>
 
--- src/components/Campaigns/index.jsx
+++ src/components/Campaigns/index.jsx
@@ -1,51 +1,57 @@
 'use client'
-import icon_natal_valor_de_uma_vida from '../../../public/images/icon-natal-valor-de-uma-vida.svg'
-import Natal_valor_de_uma_vida from '../../../public/images/cards carrosel/campaigns_Item_01.png'
-import arrow_icon_rose from '../../../public/images/btn_Icon-rose.svg'
+
 import Image from 'next/image'
 
-const arrayImages = [Natal_valor_de_uma_vida, Natal_valor_de_uma_vida, Natal_valor_de_uma_vida]
-
-export default function Campaigns() {
+export default function Campaigns({ campaign, mainColor, fontColor }) {
 
     return (
 
-        <div className="w-full  h-[411px] bg-rose-500 mt-[120px] flex items-center justify-start relative">
+        <div className={`w-full  h-[411px] bg-${mainColor} mt-[120px] flex items-center justify-start relative`}>
 
-            <div className="w-[98px] h-[67px] flex items-end justify-center box-border pb-3 rounded-tl-lg rounded-tr-lg bg-rose-500 absolute top-[-67px] left-0 ">
-                <Image className='w-[15px] mr-[10px] ' src={icon_natal_valor_de_uma_vida} alt='campanha valor de uma vida' />
-                <Image className='w-[24px]' src={icon_natal_valor_de_uma_vida} alt='campanha valor de uma vida' />
+            <div className={`w-[98px] h-[67px] flex items-end justify-center box-border pb-3 rounded-tl-lg rounded-tr-lg bg-${mainColor} absolute top-[-67px] left-0 `}>
+                <Image
+                    alt={campaign.alt}
+                    className='w-[15px] h-auto mr-[10px] ' src={campaign.icon} alt='campanha valor de uma vida'
+                />
+                
+                <Image
+                    alt={campaign.alt}
+                    className='w-[24px] h-auto' src={campaign.icon}
+                />
+
             </div>
 
-            <div className="w-[286px] h-[67px] flex items-center justify-center rounded-tl-lg rounded-tr-lg bg-rose-500 absolute top-[-67px] left-[102px] ">
+            <div className={` box-border pl-[16px] pr-[16px] h-[67px] flex items-center justify-center rounded-tl-lg rounded-tr-lg bg-${mainColor} absolute top-[-67px] left-[102px] `}>
+
                 <strong className='text-white text-[28px] font-bold font-sans shadow-black text-shadow uppercase leading-[42px]'>
-                    natal solidário
+                    {campaign.title}
                 </strong>
+
             </div>
 
             <div className='w-[366px] h-[352px]  border-r-2 border-white-500 flex flex-col items-start ml-[56px] '>
 
-                <div className='w-[292px] h-[292px] overflow-hidden mb-[23px] left-0 top-0 flex items-center justify-center rounded-[35px] border-2 border-zinc-300'>
+                <div className='w-[292px] h-[292px]  mr-[40px] overflow-hidden mb-[23px] left-0 top-0 flex items-center justify-center rounded-[35px] border-2 border-zinc-300'>
 
-                    <Image className='w-[282px] h-[272px] rounded-[35px]' src={Natal_valor_de_uma_vida} alt='icon campanha valor de uma vida' />
+                    <Image className='w-[282px] h-auto rounded-[35px]' src={campaign.photos[0]} alt='icon campanha valor de uma vida' />
 
                 </div>
 
                 <button className='w-[292px] h-[47px] bg-white rounded-[20px] flex items-center justify-between box-border pl-[30px] pr-[30px]'>
-                    <p className=' text-rose-500 h-[33px]  capitalize flex items-center text-[28px] font-bold font-sans leading-[42px]'>saber mais</p>
-                    <Image className='w-[39px]' src={arrow_icon_rose} alt='arrow_Valor_de_Uma_Vida' />
+                    <p className={`${fontColor}  h-[33px]  capitalize flex items-center text-[28px] font-bold font-sans leading-[42px]`}>saber mais</p>
+                    <Image className='w-[39px] ' src={campaign.arrowIcon} alt='arrow_Valor_de_Uma_Vida' />
                 </button>
 
             </div>
 
-            <div className='w-[82%] h-[282px] ml-[62px] flex items-center justify-around'>
+            <div className='w-[82%] h-[282px] ml-[62px] flex items-center '>
 
                 {
-                    arrayImages.map((element) => (
+                    campaign.photos.map((element, index) => (
 
-                        <div className='w-[282px] h-[282px] rounded-[35px]  overflow-hidden'>
+                        <div key={index} className='w-[290px] h-[290px] flex items-center justify-center rounded-[35px] mr-[30px] overflow-hidden'>
 
-                            <Image src={element} />
+                            <Image alt={campaign.alt} className=' w-[282px] h-[282px] rounded-[35px]' src={element} />
 
                         </div>
 
