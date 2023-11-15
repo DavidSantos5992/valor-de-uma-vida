383bba8a2f5dfc3031ab2ebb2d374dee6a5e0ec1
David Santos <david.santos@pandoradata.com.br>
2023-11-15 15:56:53 -0300

    otimização

--- src/components/BannerPartner/index.jsx
+++ src/components/BannerPartner/index.jsx
@@ -1,45 +1,35 @@
 'use client'
-import bannerEscorregador from '../../../public/images/banner_escorregador.svg';
 import Image from 'next/image';
 import { Link as ScrollLink } from "react-scroll";
-// import Button from '../Button';
 
 export default function BannerPartner() {
-
     return (
-        <div className="sm:mb-[200px] md:mb-[311px]">
+        <section className="mb-[200px] md:mb-[311px] relative">
 
-            <div className="w-full relative ">
+            <Image
+                className="w-full h-auto"
+                width={500}
+                height={500}
+                src='/images/banner_escorregador.svg'
+                alt="Banner Valor de uma vida"
+            />
 
-                <Image className="w-full h-auto" src={bannerEscorregador} alt="Banner Valor de uma vida" />
+            <div className="overlay absolute inset-0 flex flex-col items-center justify-center bg-opacity-30 bg-black">
 
-                <div className="w-full h-full flex flex-col items-center sm:justify-center box-border lg:pt-[369px]   bg-opacity-30 bg-black absolute top-0 left-0 z-10 ">
+                <p className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold font-sans uppercase leading-tight text-shadow mt-4 mb-12">Faça Parte Dessa História</p>
 
-                    <p className="text-white sm:text-[20px] md:text-[44px] lg:text-[67px] font-bold font-sans uppercase leading-[100.50px] text-shadow sm:mt-[34px] md:mb-[124px] ">Faça Parte Dessa História</p>
-
-                    {/* 
-                    <Button
-                        className='sm:w-[226.56px] sm:h-[27.33px] lg:w-[555.37px] lg:h-[67px] shadow-inner bg-yellow-400 shadow-black rounded-[39.59px] justify-center items-center text-black sm:text-[16px] md:text-[21px] font-bold font-sans uppercase '
-                        title={'Seja nosso parceiro'}
-                    /> */}
-
-                    <ScrollLink
-                        className='cursor-pointer sm:w-[226.56px] sm:h-[27.33px] lg:w-[555.37px] lg:h-[67px] shadow-inner bg-yellow-400 hover:bg-yellow-500  shadow-black rounded-[39.59px]  flex justify-center items-center text-black sm:text-[16px] md:text-[21px] font-bold font-sans uppercase '
-                        to='contato'
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                    >
-                        Seja nosso parceiro
-                    </ScrollLink>
-
-
-
-
-                </div>
+                <ScrollLink
+                    className='cursor-pointer btn-yellow text-black shadow-inner hover:bg-yellow-500 rounded-full px-8 py-3 text-xl font-bold font-sans uppercase'
+                    to='contato'
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                >
+                    Seja nosso parceiro
+                </ScrollLink>
 
             </div>
 
-        </div>
-    )
+        </section>
+    );
 }
